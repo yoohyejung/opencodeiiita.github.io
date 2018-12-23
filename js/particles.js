4c9c85a9ce82374ae9c632c852435e8fdfd75aba
Tlazypanda <itm2017004@iiita.ac.in>
2018-12-23 21:08:21 +0530

    Title added

--- js/particles.js
+++ js/particles.js
@@ -14,12 +14,12 @@ particlesJS("particles-js", {
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
-      value: 5,
+      value: 7,
       random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
     line_linked: {
-      enable: true,
+      enable: false,
       distance: 150,
       color: "#ffffff",
       opacity: 0.4,
